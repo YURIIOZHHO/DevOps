2ec612f038b5252123e72ac8a413ae9a41113702
Yurii Ozhho <yura94ozhgo@gmail.com>
2025-11-12 22:49:48 +0200

    nginx load balancer project

---
+++ NGINX/nginx-load-balancing/backend2/backend2.py
@@ -0,0 +1,12 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+
+    headers = dict(request.headers)
+    headers_html = "<br>".join([f"{k}: {v}" for k, v in headers.items()])
+    return f"<h1>Hello, I'm backend2</h1><h2>Request headers:</h2>{headers_html}"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5002)
